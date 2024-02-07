4ff78b049d7acb5b9e07e7db9cde95d551602bf5
manuel-vl <manuel.valencia@mercadolibre.com.co>
2024-02-07 13:35:58 -0500

    Initial commit

---
+++ src/main/java/com/morceapi/morceconvert/MorceconvertApplication.java
@@ -0,0 +1,13 @@
+package com.morceapi.morceconvert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorceconvertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorceconvertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morceapi/morceconvert/MorceconvertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morceapi.morceconvert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorceconvertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
